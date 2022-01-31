b8af747f629cdea384d3ff256512387ce6d38ee5
Josh Kim <joshua940308@gmail.com>
2022-01-31 09:55:29 +0900

    exercise: nested array

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -10,10 +10,20 @@
  */
 
 function nestedAdd(array) {
-  // write code here
+  let sum = 0;
+
+  for (const el of array) {
+    if (Array.isArray(el)) {
+      sum += nestedAdd(el);
+    } else {
+      sum += el;
+    }
+  }
+
+  return sum;
 }
 
-test.skip("nested arrays addition", () => {
+test("nested arrays addition", () => {
   expect(nestedAdd([1, 2, 3])).toEqual(6);
   expect(nestedAdd([1, [2], 3])).toEqual(6);
   expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
