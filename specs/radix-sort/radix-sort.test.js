836c2c7f54248ab2a8dd4eac67916f7c22bf71e2
Josh Kim <joshua940308@gmail.com>
2022-02-17 01:14:56 -0500

    exercise: radix sort

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -9,13 +9,61 @@
 
 */
 
-function radixSort(array) {
-  // code goes here
+function findBiggestNumber(array) {
+  let biggest_number = array[0];
+  for (const e of array) {
+    if (e > biggest_number) {
+      biggest_number = e;
+    }
+  }
+
+  return biggest_number;
+}
+
+function getNthPlaceNum(num, n) {
+  // num: 25, n: 1, return 5
+  // num: 25, n: 2, return 2
+  // num: 25, n: 3, return 0
+  const splitted_num = String(num).split("");
+  const num_len = splitted_num.length;
+
+  return n > num_len ? 0 : splitted_num[num_len - n];
+}
+
+function getIterationNum(array) {
+  return findBiggestNumber(array).toString().split("").length;
+}
+
+function radixSort(nums) {
+  // Plan:
+  // create buckets which will be a nested array
+  // find the biggest number which will define n iterations we need
+  // loop n times starting from 10, increment by 10 ^ n
+  // num % 10
+  // num % 100
+  // num % 1000
+  // iterate through the array and assign each number to a bucket
+  // once every number is assigned to a bucket, put the numbers back to an array
+
+  // if biggest number is 100, we have to iterate 3 times
+  const iteration_num = getIterationNum(nums);
+
+  for (let n = 1; n <= iteration_num; n += 1) {
+    const bucket = [[], [], [], [], [], [], [], [], [], []]; // 0 ~ 9 (10 buckets)
+    for (const num of nums) {
+      let idx = getNthPlaceNum(num, n);
+      bucket[idx].push(num);
+    }
+
+    nums = bucket.flat();
+  }
+
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("radix sort", function () {
+describe("radix sort", function () {
   it("should sort correctly", () => {
     const nums = [
       20, 51, 3, 801, 415, 62, 4, 17, 19, 11, 1, 100, 1244, 104, 944, 854, 34,
@@ -33,6 +81,6 @@ describe.skip("radix sort", function () {
       .fill()
       .map(() => Math.floor(Math.random() * 500000));
     const ans = radixSort(nums);
-    expect(ans).toEqual(nums.sort());
+    expect(ans).toEqual(nums.sort((a, b) => a - b));
   });
 });
