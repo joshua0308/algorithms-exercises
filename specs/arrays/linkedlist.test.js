ae2ac44d0122f445da80bce1bedb3d0214c545e5
Josh Kim <joshua940308@gmail.com>
2022-02-20 19:19:47 -0500

    exercise: linked list

--- specs/arrays/linkedlist.test.js
+++ specs/arrays/linkedlist.test.js
@@ -26,59 +26,121 @@
 class LinkedList {
   constructor() {
     this.head = null;
-    this.tail = null;
     this.length = 0;
   }
+
   push(value) {
+    // PLAN:
+    // create node
+
+    // if entry point is null
+    // set entry point to node
+
+    // if entry point points to a next node
+    // goto the next node
+    // keep repeating until there is no next node
+    // point the lastest node to the target node
     const node = new Node(value);
-    this.length++;
-    if (!this.head) {
+
+    this.length += 1;
+
+    if (this.head === null) {
       this.head = node;
     } else {
-      this.tail.next = node;
-    }
-    this.tail = node;
-  }
-  pop() {
-    return this.delete(this.length - 1);
-  }
-  _find(index) {
-    if (index >= this.length) return null;
-    let current = this.head;
-    for (let i = 0; i < index; i++) {
-      current = current.next;
-    }
-
-    return current;
-  }
-  get(index) {
-    const node = this._find(index);
-    if (!node) return void 0;
-    return node.value;
-  }
-  delete(index) {
-    if (index === 0) {
-      const head = this.head;
-      if (head) {
-        this.head = head.next;
-      } else {
-        this.head = null;
-        this.tail = null;
+      let curNode = this.head;
+      while (curNode.next) {
+        curNode = curNode.next;
       }
-      this.length--;
-      return head.value;
+
+      curNode.next = node;
+    }
+  }
+
+  pop() {
+    // PLAN:
+    // if entry point is null, return null
+    // while cur.next
+    // move to the next node
+    // when cur.next is null
+    // set the prev to null
+    // decrement length
+    // and return cur
+
+    if (this.head === null) return null;
+    this.length -= 1;
+
+    if (this.head.next === null) {
+      const popped_node = this.head;
+      this.head = null;
+      return popped_node.value;
     }
 
-    const node = this._find(index - 1);
-    const excise = node.next;
-    if (!excise) return null;
-    node.next = excise.next;
-    if (!node.next) this.tail = node.next;
-    this.length--;
-    return excise.value;
+    let prevNode = this.head;
+    let curNode = this.head.next;
+
+    while (curNode.next) {
+      prevNode = curNode;
+      curNode = curNode.next;
+    }
+
+    prevNode.next = null;
+    return curNode.value;
+  }
+
+  get(n) {
+    // PLAN:
+    // iterate through linked list n times
+    // return nth node
+
+    if (n === 0) return this.head.value;
+
+    let curNode = this.head;
+    let counter = 0;
+    while (curNode.next) {
+      curNode = curNode.next;
+      counter += 1;
+
+      if (n === counter) {
+        return curNode.value;
+      }
+    }
+
+    return null;
+  }
+
+  delete(n) {
+    // PLAN:
+    // set prevNode and curNode
+    // if curNode's index equals to n,
+    // point prevNode to curNode's next
+    // if curNode's next does not exist, point to null
+
+    let prevNode = this.head;
+    let curNode = this.head.next;
+    let counter = 1;
+
+    if (n === 0) {
+      this.head = this.head.next;
+      this.length -= 1;
+    }
+
+    if (n === 1) {
+      this.head.next = null;
+      this.length -= 1;
+    }
+
+    while (curNode.next) {
+      prevNode = curNode;
+      curNode = curNode.next;
+      counter += 1;
+      if (n === counter) {
+        prevNode.next = curNode.next ? curNode.next : null;
+        this.length -= 1;
+        return;
+      }
+    }
   }
 }
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -110,34 +172,69 @@ describe("LinkedList", function () {
 
   test("pop", () => {
     abcRange(13).map((character) => list.push(character));
+
     expect(list.length).toEqual(13);
+
     range(10).map(() => list.pop());
+
     expect(list.length).toEqual(3);
     expect(list.pop()).toEqual("c");
   });
 
+  test("pop 2", () => {
+    abcRange(2).map((character) => list.push(character));
+
+    expect(list.length).toEqual(2);
+    expect(list.pop()).toEqual("b");
+
+    expect(list.length).toEqual(1);
+    expect(list.pop()).toEqual("a");
+
+    expect(list.length).toEqual(0);
+    expect(list.pop()).toEqual(null);
+  });
+
   test("get", () => {
     list.push("first");
+
     expect(list.get(0)).toEqual("first");
+
     list.push("second");
     expect(list.get(1)).toEqual("second");
     expect(list.get(0)).toEqual("first");
+
     abcRange(26).map((character) => list.push(character));
+
     expect(list.get(27)).toEqual("z");
     expect(list.get(0)).toEqual("first");
     expect(list.get(9)).toEqual("h");
+
     list.pop();
+
     expect(list.get(list.length - 1)).toEqual("y");
+    expect(list.get(100)).toEqual(null);
   });
 
   test("delete", () => {
     abcRange(26).map((character) => list.push(character));
+
     list.delete(13);
+
     expect(list.length).toEqual(25);
     expect(list.get(12)).toEqual("m");
     expect(list.get(13)).toEqual("o");
+
     list.delete(0);
+
     expect(list.length).toEqual(24);
     expect(list.get(0)).toEqual("b");
+  });
+
+  test("delete 2", () => {
+    abcRange(5).map((character) => list.push(character));
+
+    list.delete(2);
+
+    expect(list.length).toEqual(4);
   });
 });
